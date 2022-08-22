982957378a7c43624b15cb06807a2f1910e556c0
SalamunHasan69e <salamun2600@gmail.com>
2022-08-22 13:14:56 +0600

    player list added by button

--- js/players.js
+++ js/players.js
@@ -1,3 +1,31 @@
-function addToList() {
-  console.log('Hey')
+const playerArray = [];
+function list(playerList) {
+  console.log(playerList);
+  const tableBody = document.getElementById('player-name');
+  tableBody.innerHTML = '';
+  for (let i = 0; i < playerList.length; i++) {
+    const name = playerArray[i].playerName;
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <th>${i + 1}</th>
+    <td>${name}</td>
+    `;
+    tableBody.appendChild(tr);
+  }
 }
+function addToList(player) {
+  const playerName = player.parentNode.parentNode.children[0].innerText;
+  const playerObj = {
+    playerName: playerName
+  }
+  playerArray.push(playerObj);
+  // console.log(playerArray);
+  document.getElementById('total-selected-player').innerText = playerArray.length;
+  list(playerArray);
+
+}
+
+
+// If(condition){add child}
+// else{alert()
+// Return} 
